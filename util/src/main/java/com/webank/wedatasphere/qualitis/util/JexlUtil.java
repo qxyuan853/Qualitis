7b17cddb720d5d070ede031411f247c588c55899
qxyuan853 <142065397+qxyuan853@users.noreply.github.com>
2026-06-03 19:29:36 -0400

    Evaluate JEXL expressions with restricted permissions
    Build the JEXL engine via JexlBuilder with JexlPermissions.RESTRICTED and
    upgrade commons-jexl3 to 3.6.2 (latest).

--- util/src/main/java/com/webank/wedatasphere/qualitis/util/JexlUtil.java
+++ util/src/main/java/com/webank/wedatasphere/qualitis/util/JexlUtil.java
@@ -5,14 +5,15 @@ import org.apache.commons.jexl3.JexlContext;
 import org.apache.commons.jexl3.JexlEngine;
 import org.apache.commons.jexl3.JexlExpression;
 import org.apache.commons.jexl3.MapContext;
-import org.apache.commons.jexl3.internal.Engine;
+import org.apache.commons.jexl3.JexlBuilder;
+import org.apache.commons.jexl3.introspection.JexlPermissions;
 
 /**
  * @author allenzhou@webank.com
  * @date 2021/8/17 10:45
  */
 public class JexlUtil {
-    private static JexlEngine jexlEngine = new Engine();
+    private static JexlEngine jexlEngine = new JexlBuilder().permissions(JexlPermissions.RESTRICTED).create();
 
     public static synchronized Object executeExpression(String jexlExpression, Map<String, Object> map) {
         JexlExpression expression = jexlEngine.createExpression(jexlExpression);
